103530cdd0f2be0cec565880831843373ddbbedc
MisLink <gjq.uoiai@outlook.com>
2017-04-17 23:12:30 +0800

    Change output encoding to utf-8.
    Signed-off-by: MisLink <gjq.uoiai@outlook.com>

--- profile.ps1
+++ profile.ps1
@@ -1,5 +1,5 @@
 [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
-$OutputEncoding = New-Object -typename System.Text.UTF8Encoding
+$outputencoding = [System.Text.Encoding]::GetEncoding("utf-8")
 . (Resolve-Path "$env:LOCALAPPDATA\GitHub\shell.ps1")
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 # Load posh-git module from current directory
