864539d62778ac0083336c895540ebf2e768b9d6
MisLink <gjq.uoiai@outlook.com>
2017-06-24 08:38:51 +0800

    Add alias.
    Signed-off-by: MisLink <gjq.uoiai@outlook.com>

--- profile.ps1
+++ profile.ps1
@@ -1,10 +1,12 @@
 $OutputEncoding = New-Object -typename System.Text.UTF8Encoding
 # $outputencoding = [System.Text.Encoding]::GetEncoding("utf-8")
-# [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
-. (Resolve-Path "$env:LOCALAPPDATA\GitHub\shell.ps1")
+[Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 # Load posh-git module from current directory
 Import-Module posh-git
+
+$scripts = resolve-path ~/Documents/dotfiles
+Import-Module "Pscx"
 # Get full name of user
 $username = [Environment]::UserName
 $hostname = $env:COMPUTERNAME
@@ -16,23 +18,24 @@ $adm = [System.Security.Principal.WindowsBuiltInRole]::Administrator
 $IsAdmin = $prp.IsInRole($adm)
 
 # Function to write git repository status on prompt
-function writegitprompt($status){
+function writegitprompt($status) {
     if ($status) {
         write-host ' (' -nonewline
         write-host ($status.Branch) -nonewline
         write-host ' ' -nonewline
-        if($status.HasWorking) {
-            write-host "$([char]0x25CF)" -nonewline -ForegroundColor @{$true='Green';$false='DarkGray'}[$status.Working.Added -and $status.Working.Added.Count -ge 0]
-            write-host "$([char]0x25CF)" -nonewline -ForegroundColor @{$true=(@{$true='Red';$false='Yellow'}[$status.Working.Unmerged -and $status.Working.Unmerged.Count -ge 0]);$false=(@{$true='Red';$false='DarkGray'}[$status.Working.Unmerged -and $status.Working.Unmerged.Count -ge 0])}[$status.Working.Modified -and $status.Working.Modified.Count -ge 0]
-            write-host "$([char]0x25CF)" -nonewline -ForegroundColor @{$true='Red';$false='DarkGray'}[$status.Working.Deleted -and $status.Working.Deleted.Count -ge 0]
-        } else {
+        if ($status.HasWorking) {
+            write-host "$([char]0x25CF)" -nonewline -ForegroundColor @{$true = 'Green'; $false = 'DarkGray'}[$status.Working.Added -and $status.Working.Added.Count -ge 0]
+            write-host "$([char]0x25CF)" -nonewline -ForegroundColor @{$true = (@{$true = 'Red'; $false = 'Yellow'}[$status.Working.Unmerged -and $status.Working.Unmerged.Count -ge 0]); $false = (@{$true = 'Red'; $false = 'DarkGray'}[$status.Working.Unmerged -and $status.Working.Unmerged.Count -ge 0])}[$status.Working.Modified -and $status.Working.Modified.Count -ge 0]
+            write-host "$([char]0x25CF)" -nonewline -ForegroundColor @{$true = 'Red'; $false = 'DarkGray'}[$status.Working.Deleted -and $status.Working.Deleted.Count -ge 0]
+        }
+        else {
             write-host "$([char]0x25CF)$([char]0x25CF)$([char]0x25CF)" -nonewline -ForegroundColor DarkGray
         }
-        if($status.HasIndex) {
+        if ($status.HasIndex) {
             write-host "|" -nonewline
-            write-host "$([char]0x25CF)" -nonewline -ForegroundColor @{$true='Green';$false='DarkGray'}[$status.Index.Added -and $status.Index.Added.Count -ge 0]
-            write-host "$([char]0x25CF)" -nonewline -ForegroundColor @{$true=(@{$true='Red';$false='Yellow'}[$status.Index.Unmerged -and $status.Index.Unmerged.Count -ge 0]);$false=(@{$true='Red';$false='DarkGray'}[$status.Index.Unmerged -and $status.Index.Unmerged.Count -ge 0])}[$status.Index.Modified -and $status.Index.Modified.Count -ge 0]
-            write-host "$([char]0x25CF)" -nonewline -ForegroundColor @{$true='Red';$false='DarkGray'}[$status.Index.Deleted -and $status.Index.Deleted.Count -ge 0]
+            write-host "$([char]0x25CF)" -nonewline -ForegroundColor @{$true = 'Green'; $false = 'DarkGray'}[$status.Index.Added -and $status.Index.Added.Count -ge 0]
+            write-host "$([char]0x25CF)" -nonewline -ForegroundColor @{$true = (@{$true = 'Red'; $false = 'Yellow'}[$status.Index.Unmerged -and $status.Index.Unmerged.Count -ge 0]); $false = (@{$true = 'Red'; $false = 'DarkGray'}[$status.Index.Unmerged -and $status.Index.Unmerged.Count -ge 0])}[$status.Index.Modified -and $status.Index.Modified.Count -ge 0]
+            write-host "$([char]0x25CF)" -nonewline -ForegroundColor @{$true = 'Red'; $false = 'DarkGray'}[$status.Index.Deleted -and $status.Index.Deleted.Count -ge 0]
         }
         write-host ')' -nonewline
     }
@@ -41,10 +44,11 @@ function writegitprompt($status){
 # Here we go
 function global:prompt {
     $realLASTEXITCODE = $LASTEXITCODE
-    if($IsAdmin){
-        write-host "gjq.uoiai" -nonewline -ForegroundColor Red
-    } else {
-        write-host "gjq.uoiai" -nonewline -ForegroundColor Yellow
+    if ($IsAdmin) {
+        write-host $username -nonewline -ForegroundColor Red
+    }
+    else {
+        write-host $username -nonewline -ForegroundColor Yellow
     }
     write-host " " -nonewline -ForegroundColor Gray
     Write-Host($pwd.ProviderPath) -nonewline -ForegroundColor Green
@@ -57,13 +61,13 @@ function global:prompt {
     $saveX = [console]::CursorLeft
     $columns = (Get-Host).UI.RawUI.windowsize.width    # Column quantity of console window
     # [console]::SetCursorPosition($columns - 8, $saveY)
-    [console]::SetCursorPosition($saveX+1, $saveY)
+    [console]::SetCursorPosition($saveX + 1, $saveY)
     write-host "[" -nonewline
     write-host (Get-Date -format "HH:mm:ss") -nonewline -ForegroundColor Cyan
     # write-host "]" -nonewline
     # [console]::setcursorposition($saveX,$saveY)        # Move cursor back
     write-host "]" -nonewline
-    [console]::setcursorposition(0,$saveY+1)
+    [console]::setcursorposition(0, $saveY + 1)
     $global:LASTEXITCODE = $realLASTEXITCODE
     return " $([char]0x3BB) "
     # return " $([char]0x2764) "
@@ -71,7 +75,7 @@ function global:prompt {
 
 # alias
 function pip_update {
-     pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
+    pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
 }
 
 function pip3_update {
@@ -80,7 +84,10 @@ function pip3_update {
 
 Set-Alias pip-update pip_update
 Set-Alias pip3-update pip3_update
-Set-Alias sudo csudo
+if (test-path alias:\set) { remove-item alias:\set -force }
+set-alias set set-variableEx -force
+set-alias unset remove-variable
+set-alias sudo Invoke-Elevated
 
 if (Test-Path ".\venv\Scripts\activate") {
     .\venv\Scripts\activate.ps1
