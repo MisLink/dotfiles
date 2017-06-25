f0565ec3f223b3a49230efc3d45ba62f32b79032
MisLink <gjq.uoiai@outlook.com>
2017-06-26 00:57:06 +0800

    Add alias. Modify prompt.
    Signed-off-by: MisLink <gjq.uoiai@outlook.com>

--- profile.ps1
+++ profile.ps1
@@ -68,7 +68,8 @@ function global:prompt {
     write-host "]" -nonewline
     [console]::setcursorposition(0, $saveY + 1)
     $global:LASTEXITCODE = $realLASTEXITCODE
-    return " $([char]0x3BB) "
+    return " :) "
+    # return " $([char]0x3BB) "
     # return " $([char]0x2764) "
 }
 
