eb03fb4a13316493c3505ecde683cd838626e92c
MisLink <gjq.uoiai@outlook.com>
2017-10-02 21:57:30 +0800

    Add new zsh plugins.
    Signed-off-by: MisLink <gjq.uoiai@outlook.com>

--- setup.sh
+++ setup.sh
@@ -1,16 +1,5 @@
 #!/bin/zsh
 
-# zsh
-if [[ ! -d ~/.oh-my-zsh ]]; then
-    echo "Install oh-my-zsh ..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
-
-cp -v zsh/ys.zsh-theme ~/.oh-my-zsh/custom/themes
-cp -v zsh/alias ~/.alias
-cp -v zsh/zshrc ~/.zshrc
-source ~/.zshrc
-
 # vim
 cp -v vim/vimrc ~/.vimrc
 
@@ -23,3 +12,24 @@ cp -v git/gitconfig ~/.gitconfig
 # ssh
 cp -v ssh/config ~/.ssh/config
 mkdir -p ~/.ssh/socks
+
+# zsh
+if [[ ! -d ~/.oh-my-zsh ]]; then
+    echo "Install oh-my-zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+if [[ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]]; then
+    echo "Install zsh-autosuggestions..."
+    git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+
+if [[ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]]; then
+    echo "Install zsh-syntax-highlighting..."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
+cp -v zsh/ys.zsh-theme ~/.oh-my-zsh/custom/themes
+cp -v zsh/alias ~/.alias
+cp -v zsh/zshrc ~/.zshrc
+source ~/.zshrc
