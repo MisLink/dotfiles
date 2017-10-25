1a0cc418d1db6352abf86b29a92810d5d86a0489
MisLink <gjq.uoiai@outlook.com>
2017-10-25 13:53:58 +0800

    Auto commit at 2017-10-25 13:53:58.
    Signed-off-by: MisLink <gjq.uoiai@outlook.com>

---
+++ auto-commit.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+auto_commit() {
+    (FILE_PATH=$1;
+    cd $FILE_PATH;
+    GIT_PATH=`which git`;
+    $GIT_PATH add .;
+    $GIT_PATH cs -m "Auto commit at `date +'%Y-%m-%d %H:%M:%S'`.";
+    $GIT_PATH push;)
+}
