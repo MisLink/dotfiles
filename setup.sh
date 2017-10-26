101acbb0ff11fe916abe9a9c9f67a7e9eb79b7f5
MisLink <gjq.uoiai@outlook.com>
2017-10-26 16:29:08 +0800

    Add useful alias.
    Signed-off-by: MisLink <gjq.uoiai@outlook.com>

--- setup.sh
+++ setup.sh
@@ -1,18 +1,5 @@
 #!/bin/zsh
 
-# vim
-cp -v vim/vimrc ~/.vimrc
-
-# tmux
-cp -v tmux/tmux.conf ~/.tmux.conf
-
-# gitconfig
-cp -v git/gitconfig ~/.gitconfig
-
-# ssh
-cp -v ssh/config ~/.ssh/config
-mkdir -p ~/.ssh/socks
-
 # zsh
 if [[ ! -d ~/.oh-my-zsh ]]; then
     echo "Install oh-my-zsh..."
@@ -32,3 +19,16 @@ fi
 cp -v zsh/ys.zsh-theme ~/.oh-my-zsh/custom/themes
 cp -v zsh/alias ~/.alias
 cp -v zsh/zshrc ~/.zshrc
+
+# vim
+cp -v vim/vimrc ~/.vimrc
+
+# tmux
+cp -v tmux/tmux.conf ~/.tmux.conf
+
+# gitconfig
+cp -v git/gitconfig ~/.gitconfig
+
+# ssh
+cp -v ssh/config ~/.ssh/config
+mkdir -p ~/.ssh/socks
