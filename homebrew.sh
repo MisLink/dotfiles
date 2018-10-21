d7a3833a24e70abc124cce657c4d7d2dac0dc5c9
MisLink <gjq.uoiai@outlook.com>
2018-10-22 01:28:31 +0800

    Modify homebrew source.
    Signed-off-by: MisLink <gjq.uoiai@outlook.com>

--- homebrew.sh
+++ homebrew.sh
@@ -5,6 +5,15 @@ if ! which brew &>/dev/null; then
     echo "Install homebrew..."
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+brew update
+export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
+cd "$(brew --repo)"
+git remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
+cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+git remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
+brew update
+
+brew install ctags
 brew install zsh
 brew install vim
 brew install git
