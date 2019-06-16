851ba4b589513f1c28fd6a0231948c5af4d29f19
MisLink <gjq.uoiai@outlook.com>
2019-06-16 23:28:50 +0800

    Change homebrew.
    Signed-off-by: MisLink <gjq.uoiai@outlook.com>

--- homebrew.sh
+++ homebrew.sh
@@ -16,7 +16,6 @@ brew update
 # add tap
 brew tap homebrew/cask-fonts
 
-brew install aria2
 brew install ctags
 brew install zsh
 brew install vim
@@ -24,13 +23,11 @@ brew install git
 brew install trash
 brew install pandoc
 brew install wrk
-brew install supervisor
 brew install watch
 # cask
 brew cask install telegram
 brew cask install google-chrome
 brew cask install visual-studio-code
-brew cask install authy
 brew cask install typora
 brew cask install gitkraken
 brew cask install jetbrains-toolbox
@@ -41,7 +38,7 @@ brew cask install bettertouchtool
 brew cask install appcleaner
 brew cask install yinxiangbiji
 brew cask install shadowsocksx-ng
-brew cask install the-unarchiver
+brew cask install keka
 brew cask install onedrive
 brew cask install iterm2
 brew cask install battle-net
@@ -49,7 +46,7 @@ brew cask install jietu
 brew cask install itsycal
 brew cask install kindle
 brew cask install neteasemusic
-brew cask install qqmusic
+brew cask install spotify
 brew cask install qq
 brew cask install wechat
 brew cask install parallels
@@ -58,7 +55,4 @@ brew cask install iina
 brew cask install cakebrew
 brew cask install qlcolorcode qlimagesize qlmarkdown qlstephen quicklook-json
 # font
-brew cask install font-sarasa-gothic
-brew cask install font-source-han-sans
-brew cask install font-source-han-serif-el-m
-brew cask install font-source-han-serif-sb-h
+brew cask install font-sarasa-gothic font-source-han-sans font-source-han-serif-el-m font-source-han-serif-sb-h
