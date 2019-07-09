fd61ae511c690bfe3465db84d4aa8bd0639eac1d
MisLink <gjq.uoiai@outlook.com>
2019-07-09 21:20:38 +0800

    Add container.sh.
    Signed-off-by: MisLink <gjq.uoiai@outlook.com>

---
+++ container.sh
@@ -0,0 +1,8 @@
+docker run --name=redis -p 6379:6379 --restart=always -d redis
+docker run --name=mysql_8 -e MYSQL_ROOT_PASSWORD=root -p 3306:3306 --restart=always -d mysql
+docker run --name=postgres_10 -e POSTGRES_PASSWORD=root -p 5432:5432 --restart=always -d postgres
+docker run --name=rabbitmq -p 15672:15672 --restart=always -d rabbitmq:3-management
+docker run --name=zookeeper -p 8080:8080 -p 3888:3888 -p 2888:2888 -p 2181:2181 --restart=always -d zookeeper
+
+docker run --name=aria2 -v <>:/app/Downloads -p 6800:6800 --restart=always -d gjquoiai/aria2:latest
+docker run --name=aria2_web --restart always -d -p 8000:8888 -v <>:/aria2 -w /aria2 python:3.6 python -m http.server 8000 -b 0.0.0.0
