e5af8d58d3e29b265150f3c4f17d367bd67eeb1a
MisLink <gjq.uoiai@outlook.com>
2020-06-13 22:50:32 +0800

    Update
    Signed-off-by: MisLink <gjq.uoiai@outlook.com>

--- container.sh
+++ container.sh
@@ -1,8 +1,8 @@
 # db
-docker run --name=redis -p 6379:6379 --restart=always -d redis:5
+docker run --name=redis -p 6379:6379 --restart=always -d redis:6
 docker run --name=mysql -e MYSQL_ROOT_PASSWORD=root -p 3306:3306 --restart=always -d mysql:8
-docker run --name=postgres -e POSTGRES_PASSWORD=root -p 5432:5432 --restart=always -d postgres:11
+docker run --name=postgres -e POSTGRES_PASSWORD=root -p 5432:5432 --restart=always -d postgres:12
 docker run --name=mongo -p 27017:27017 --restart=always -d mongo:4
 docker run --name=rabbitmq -p 15672:15672 -p 5672:5672 --restart=always -d rabbitmq:3-management
 docker run --name=aria2 -p 6800:6800 --restart=always -v :/app/Downloads -d gjquoiai/aria2:latest
-docker run --name=elasticsearch -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" --restart=always -d elastic/elasticsearch:6.8.7
+docker run --name=elasticsearch -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" --restart=always -d elasticsearch:6.8.10
